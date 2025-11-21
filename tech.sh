a22344eb8e509b745519470bb40106b235b4b989
x404x <206167903+a404k@users.noreply.github.com>
2025-11-21 17:17:56 -0300

    Update tech.sh

--- tech.sh
+++ tech.sh
@@ -12,7 +12,6 @@ for i in $(seq 1 30); do
     echo -ne "${Y}.${NC}"
     sleep 0.1
 done
-echo -e " ${G}[OK]${NC}"
 
 clear
 
@@ -62,25 +61,48 @@ cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 p "Instalando plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
 
 p "Modificando .zshrc..."
 
-cat >> ~/.zshrc << 'EOF'
+cat > ~/.zshrc << 'EOF'
+
+plugins=(
+  git
+  zsh-completions
+  zsh-history-substring-search
+  zsh-autosuggestions
+  fast-syntax-highlighting
+  extract
+  sudo
+)
 
 neofetch
 
-source ~/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/.oh-my-zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
-source ~/.oh-my-zsh/plugins/zsh-completions/zsh-completions.plugin.zsh
-
-alias update='pkg update && pkg upgrade'
-alias cls='clear'
+alias update='pkg update && pkg upgrade -y'
+alias c='clear'
 alias ls='lsd'
+alias ll='lsd -l'
+alias la='lsd -a'
 alias cat='bat'
+alias e='exit'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias pi='pkg install'
+alias pu='pkg uninstall'
+alias home='cd ~'
+alias descargas='cd ~/storage/downloads'
+alias sdcard='cd ~/storage/shared'
+alias termux='cd /data/data/com.termux/files/home'
+alias ts='termux-setup-storage'
+alias wp='termux-wake-lock'
+alias wpo='termux-wake-unlock'
+alias ip='ip -c a'
+alias myip='curl ifconfig.me'
+alias help='echo -e "\n\033[1;36mAlias personales:\033[0m\n"; echo -e "\033[1;33mupdate\033[0m = pkg update && pkg upgrade -y"; echo -e "\033[1;33mc\033[0m = clear"; echo -e "\033[1;33mls\033[0m = lsd"; echo -e "\033[1;33mll\033[0m = lsd -l"; echo -e "\033[1;33mla\033[0m = lsd -a"; echo -e "\033[1;33mcat\033[0m = bat"; echo -e "\033[1;33me\033[0m = exit"; echo -e "\033[1;33m..\033[0m = cd .."; echo -e "\033[1;33m...\033[0m = cd ../.."; echo -e "\033[1;33m....\033[0m = cd ../../.."; echo -e "\033[1;33mpi\033[0m = pkg install"; echo -e "\033[1;33mpu\033[0m = pkg uninstall"; echo -e "\033[1;33mhome\033[0m = cd ~"; echo -e "\033[1;33mdescargas\033[0m = cd ~/storage/downloads"; echo -e "\033[1;33msdcard\033[0m = cd ~/storage/shared"; echo -e "\033[1;33mtermux\033[0m = cd /data/data/com.termux/files/home"; echo -e "\033[1;33mts\033[0m = termux-setup-storage"; echo -e "\033[1;33mwp\033[0m = termux-wake-lock"; echo -e "\033[1;33mwpo\033[0m = termux-wake-unlock"; echo -e "\033[1;33mip\033[0m = ip -c a"; echo -e "\033[1;33mmyip\033[0m = curl ifconfig.me"'
 
 EOF
 
@@ -93,14 +115,9 @@ else
     echo -e "${R}ERROR: No se encontró el archivo AEF en esta carpeta.${NC}"
 fi
 
-p "Estableciendo ZSH como shell predeterminado..."
+p "Estableciendo shell predeterminado..."
 
-PASSWD="$PREFIX/etc/passwd"
-USER=$(whoami)
-
-if grep -q "^${USER}:" "$PASSWD"; then
-    sed -i "s|:/data/data/com.termux/files/usr/bin/login|:/data/data/com.termux/files/usr/bin/zsh|" "$PASSWD"
-fi
+chsh -s zsh
 
 p "Limpiando historial..."
 rm -rf ~/.bash_history
